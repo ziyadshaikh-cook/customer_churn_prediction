427666cb36d7c1b071a28483436e92c41b82131b
Ziyad Shaikh <ziyadshaikh98@gmail.com>
2026-05-10 00:22:30 +0530

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='customer_churn_prediction',
+    version='0.0.1',
+    author='Ziyad Shaikh',
+    author_email='ziyadshaikh98@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
